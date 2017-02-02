1c40e952cf276e3d464a456e6fbff81d954155ba
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-02 13:16:18 +0700

    install simple express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+const express = require('express');
+
+const app = new express();
+const port = process.env.PORT || 3002;
+
+app.get('/', function(req, res){
+    res.send("Hello world");
+})
+
+app.listen(port, function(err){
+    if (err) {
+        console.log(err);
+    }
+
+    console.log("app is now listen on port", port);
+});
