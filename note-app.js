f1cf12decb8ce19b5f57d45b60a5159a4a18a5c6
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-08 15:37:25 +0700

    create help command for yargs

--- note-app.js
+++ note-app.js
@@ -1,12 +1,31 @@
 const yargs = require('yargs')
 const notes = require('./playgrounds/notes')
+const _ = require('lodash')
 
-const cmd = yargs.argv
+const cmd = yargs
+              .command('add', 'add note', {
+                title: {
+                  describe: 'Title of note',
+                  demand: true,
+                  alias: 't'
+                },
+                body: {
+                  describe: 'Body of note',
+                  demand: true,
+                  alias: 'b'
+                }
+              })
+              .help()
+              .argv
 const command = cmd._[0]
 const { title, body } = cmd
 
 try {
-    notes[command](cmd)
-} catch(e) {
-    console.log('error ', e)
+  if (!_.has(notes, command)) {
+    throw `no command:(${command}) for note-app`
+  }
+
+  notes[command](cmd)
+} catch (e) {
+  console.log(`error -- ${e}`)
 }
