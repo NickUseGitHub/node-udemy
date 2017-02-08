e95fa7c0a3941cb2c370b3e15dce3b45835a022c
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-08 10:26:39 +0700

    install lodash

--- playgrounds/notes.js
+++ playgrounds/notes.js
@@ -1,22 +1,60 @@
 const fs = require('fs')
+const _ = require('lodash')
+const fileName = 'notes-data.js'
+
+function getAllNotes() {
+  try {
+    const notes = fs.readFileSync(`./${fileName}`, 'utf-8')
+    return notes
+  } catch (e) {
+    return []
+  }
+}
+
+function writeFile(title, body) {
+  const notes = getAllNotes()
+  const note = { title, body }
+
+  if (_.isEmpty(notes.filter(n => n.title === title))) {
+    console.log('cannot write file because note is duplicated')
+    return
+  }
+
+  notes.push(note)
+
+  try {
+    fs.writeFileSync(`./${fileName}`, JSON.stringify(notes))
+    console.log('writeFile completed!')
+    console.log('--------------------')
+  } catch (e) {
+    throw (e)
+  }
+}
+
+function get(cmd) {
+  const notes = getAllNotes()
+  const {title} = cmd
+  return _.head(notes.filter(note => note.title === title))
+}
 
 function add(cmd) {
-    console.log('addNote', cmd)
-    return 'new note'
+  const {title, body} = cmd
+  writeFile(title, body)
+  return 'new note'
 }
 
 function edit(cmd) {
-    console.log('editNote', cmd)
-    return 'update note'
+  console.log('editNote', cmd)
+  return 'update note'
 }
 
 function remove(cmd) {
-    console.log('removeNote', cmd)
-    return 'remove note'
+  console.log('removeNote', cmd)
+  return 'remove note'
 }
 
 module.exports = {
-    add,
-    edit,
-    remove
+  add,
+  edit,
+  remove
 }
