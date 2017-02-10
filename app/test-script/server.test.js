c62bad0bdba257004736d9ab893a26746304a0fa
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-10 17:54:41 +0700

    descript testing

--- app/test-script/server.test.js
+++ app/test-script/server.test.js
@@ -2,24 +2,25 @@ import requestSupertest from 'supertest'
 import app from '../server'
 import expect from 'expect'
 
+describe('Server Test', () => {
+  it('should return simple hello world', (done) => {
+    requestSupertest(app)
+      .get('/test')
+      .expect(200)
+      .expect(res => {
+        expect(res.text).toBe('Hello World').toBeA('string')
+      })
+      .end(done)
+  })
 
-it('should return simple hello world', (done) => {
-  requestSupertest(app)
-    .get('/test')
-    .expect(200)
-    .expect(res => {
-      expect(res.text).toBe('Hello World').toBeA('string')
-    })
-    .end(done)
-})
-
-it('should get include user whose name is nick', (done) => {
-  requestSupertest(app)
-    .get('/users')
-    .expect(200)
-    .expect(res => {
-      expect(res.body)
-        .toInclude({name: 'nick', age: 27})
-    })
-    .end(done)
+  it('should get include user whose name is nick', (done) => {
+    requestSupertest(app)
+      .get('/users')
+      .expect(200)
+      .expect(res => {
+        expect(res.body)
+          .toInclude({name: 'nick', age: 27})
+      })
+      .end(done)
+  })
 })
--- app/test-script/utils.test.js
+++ app/test-script/utils.test.js
@@ -4,19 +4,22 @@ import expect from 'expect'
 const num1 = 3,
       num2 = 4
 
-it('add two number', () => {
-  const res = add(num1, num2)
-  expect(res).toBeA('number')
-})
+describe('Utils', () => {
+  it('add two number', () => {
+    const res = add(num1, num2)
+    expect(res).toBeA('number')
+  })
 
-it('async multiple two number', done => {
-  asyncMultiple(num1, num2, mul => {
-    expect(mul).toBe(12).toBeA('number')
-    done()
+  it('async multiple two number', done => {
+    asyncMultiple(num1, num2, mul => {
+      expect(mul).toBe(12).toBeA('number')
+      done()
+    })
+  })
+
+  it('square two number', () => {
+    const res = square(num1)
+    expect(res).toBeA('number')
   })
 })
 
-it('square two number', () => {
-  const res = square(num1)
-  expect(res).toBeA('number')
-})
