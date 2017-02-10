1e382f83ef6e8ea6518a63f09e8eddb0b67d60d6
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-10 16:03:13 +0700

    create util function square

--- app/test-script/utils.test.js
+++ app/test-script/utils.test.js
@@ -1,5 +1,22 @@
-import {add} from '../utils/num'
+import {add, square} from '../utils/num'
+
+const num1 = 3,
+      num2 = 4
 
 it('should add two number', () => {
-  const res = add(3, 'asf')
+  const res = add(num1, num2)
+  console.log(`test add result -- ${res}`)
+
+  if (typeof res !== 'number') {
+    throw new Error(`result(${res}) not a number`)
+  }
+})
+
+it('should square two number', () => {
+  const res = square(num1)
+  console.log(`test square result -- ${res}`)
+
+  if (typeof res !== 'number') {
+    throw new Error(`result(${res}) not a number`)
+  }
 })
--- app/utils/num.js
+++ app/utils/num.js
@@ -1,3 +1,7 @@
 export function add(a, b) {
   return a + b
 }
+
+export function square(num) {
+  return Math.pow(num, 2)
+}
