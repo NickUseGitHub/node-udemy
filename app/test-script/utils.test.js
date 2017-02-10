ada93c38b97a5d62cf799f1230c58559ba2d726c
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-10 16:29:53 +0700

    add test-watch script

--- app/test-script/utils.test.js
+++ app/test-script/utils.test.js
@@ -1,22 +1,15 @@
 import {add, square} from '../utils/num'
+import expect from 'expect'
 
 const num1 = 3,
       num2 = 4
 
-it('should add two number', () => {
+it('add two number', () => {
   const res = add(num1, num2)
-  console.log(`test add result -- ${res}`)
-
-  if (typeof res !== 'number') {
-    throw new Error(`result(${res}) not a number`)
-  }
+  expect(res).toBeA('number')
 })
 
-it('should square two number', () => {
+it('square two number', () => {
   const res = square(num1)
-  console.log(`test square result -- ${res}`)
-
-  if (typeof res !== 'number') {
-    throw new Error(`result(${res}) not a number`)
-  }
+  expect(res).toBeA('number')
 })
