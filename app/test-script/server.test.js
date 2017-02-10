599b134e0599e3ddd374d6ca937b14f0b731569f
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-10 17:29:20 +0700

    use expect module package for server.test.js

--- app/test-script/server.test.js
+++ app/test-script/server.test.js
@@ -1,10 +1,14 @@
 import requestSupertest from 'supertest'
 import app from '../server'
+import expect from 'expect'
 
 
 it('should return simple hello world', (done) => {
   requestSupertest(app)
     .get('/test')
-    .expect('Hello World')
+    .expect(200)
+    .expect(res => {
+      expect(res.text).toBe('Hello World').toBeA('string')
+    })
     .end(done)
 })
