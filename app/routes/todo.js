a7c02b8fd307c68ea9388f9a7f82599d446db7be
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-15 11:27:28 +0700

    refactor routes

--- app/routes/todo.js
+++ app/routes/todo.js
@@ -3,7 +3,20 @@ import {Router} from 'express'
 const route = Router()
 
 route.get('/todo', (req, res) => {
-  res.send('This is todo!!!')
+  res.send('get all todos!!!')
+})
+
+route.get('/todo/:todoId', (req, res) => {
+  const {todoId} = req.params 
+  res.send(`get todo id:${todoId}`)
+})
+
+route.put('/todo', (req, res) => {
+  res.send('create todo!!!')
+})
+
+route.post('/todo/:todoId', (req, res) => {
+  res.send('update todo!!!')
 })
 
 export default route
