3e2fbd6e6da7495731b07ff1a036ccfb7ed41813
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-15 15:01:00 +0700

    complete todo restAPI

--- app/model/todo.js
+++ app/model/todo.js
@@ -1,20 +1,10 @@
 import mongoose, {Schema} from 'mongoose'
 
-// const kittySchema = mongoose.Schema({
-//     name: String
-// })
-// const Kitten = mongoose.model('Kitten', kittySchema)
-// const fluffy = new Kitten({ name: 'Silence' })
-// fluffy.save(function (err, fluffy) {
-//   if (err) return console.error(err)
-//   console.log('--- kitten save!! ---')
-// })
-// console.log('Hello Kitty', fluffy.name)
-
 const dbName = 'Todo'
 const schema = new Schema({
   detail: {
-    type: String, 
+    type: String,
+    trim: true,
     required: true
   },
   completed: {
--- app/routes/todo.js
+++ app/routes/todo.js
@@ -1,22 +1,56 @@
-import {Router} from 'express'
+import { Router } from 'express'
+import Todo from '../model/todo'
 
 const route = Router()
 
 route.get('/todo', (req, res) => {
-  res.send('get all todos!!!')
+  Todo.find()
+    .then(todos => res.json(todos))
+    .catch(e => res.status(400).send(e))
 })
 
 route.get('/todo/:todoId', (req, res) => {
-  const {todoId} = req.params 
-  res.send(`get todo id:${todoId}`)
+  const {todoId} = req.params
+  Todo.findById(todoId)
+    .then(todos => res.json(todos))
+    .catch(e => res.status(400).send(e))
 })
 
 route.put('/todo', (req, res) => {
-  res.send('create todo!!!')
+  const todo = new Todo(req.body)
+
+  //save todo
+  todo.save((err, td) => {
+    if (err) {
+      res.status(400)
+        .send(err)
+    } else {
+      res.json(td)
+    }
+  })
+
 })
 
 route.post('/todo/:todoId', (req, res) => {
-  res.send('update todo!!!')
+  const reqTodo = req.body.todo
+  const {todoId} = req.params
+  Todo.update({_id:todoId}, reqTodo, (err, raw) => {
+    if (raw) {
+      res.status(400).send(raw)
+    } else {
+      res.send({msg:'sucess', reqTodo})
+    }
+  })
+})
+
+route.delete('/todo/:todoId', (req, res) => {
+  const {todoId} = req.params
+  Todo.findOneAndRemove({_id: todoId}, (err, todo) => {
+    if (err) {
+      res.status(400).send(err)
+    }
+    res.json({msg: 'success', todo})
+  })
 })
 
 export default route
