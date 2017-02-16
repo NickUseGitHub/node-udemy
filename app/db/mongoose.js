fb0a43e80dee45c65b6a000f186271a55f854768
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-16 13:22:00 +0700

    fix bug dbUrl and modify package.json

--- app/db/mongoose.js
+++ app/db/mongoose.js
@@ -3,9 +3,10 @@ import mongoose from 'mongoose'
 const env = process.env.NODE_ENV || 'production'
 const dbPort = process.env.DB_PORT || 27017
 const dbName = 'nodeUdemy'
-const dbUrl = env !== 'production'? `mongodb://localhost/${dbName}` : `mongodb://mongo:${dbPort}/${dbName}`
+const dbUrl = env !== 'production'? `mongodb://mongo:${dbPort}/${dbName}` : `mongodb://localhost/${dbName}`
 
 mongoose.Promise = global.Promise
+console.log('before connect dbUrl -- ', dbUrl)
 mongoose.connect(dbUrl)
 
 mongoose.connection.on('connected', () => {
