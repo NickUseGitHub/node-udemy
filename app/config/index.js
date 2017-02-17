3e196a45af65408d9df649831477dda081981750
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-17 16:38:29 +0700

    refactor middleware log

--- app/config/index.js
+++ app/config/index.js
@@ -2,11 +2,18 @@ const testPort = 3002
       ,devPort = 3002
       ,dbPort = 27017
       ,dbName = 'nodeUdemy'
+      ,dbNameTest = 'nodeUdemyTest'
+
+console.log('************************************************')
+console.log('*')
+console.log(`**  server : ${process.env.NODE_ENV}`)
+console.log('*')
+console.log('************************************************')
 
 switch(process.env.NODE_ENV) {
   case 'test':
     process.env.PORT = testPort
-    process.env.MONGODB_URI = `mongodb://mongo:${dbPort}/${dbName}`
+    process.env.MONGODB_URI = `mongodb://mongo:${dbPort}/${dbNameTest}`
     break
   case 'development':
     process.env.PORT = devPort
--- app/middlewares/log.js
+++ app/middlewares/log.js
@@ -1,9 +1,13 @@
 export default function(req, res, next) {
-  console.log('----- log -----')
-  console.log('HTTP request baseUrl', req.baseUrl)
-  console.log('HTTP request method', req.method)
-  console.log('HTTP request from ip', req.ip)
-  console.log('HTTP request hostname', req.hostname)
-  console.log('----- end log -----')
+
+  if (process.env.NODE_ENV !== 'test') {
+    console.log('----- log -----')
+    console.log('HTTP request baseUrl', req.baseUrl)
+    console.log('HTTP request method', req.method)
+    console.log('HTTP request from ip', req.ip)
+    console.log('HTTP request hostname', req.hostname)
+    console.log('----- end log -----')
+  }
+  
   next()
 }
