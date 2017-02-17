513fdc1ddfbde0dac95ef652b4ffffbe6e9bca1b
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-17 17:17:41 +0700

    install validator package & refactor schema user

--- app/model/user.js
+++ app/model/user.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from 'mongoose'
+import validator from 'validator'
 
 const documentName = 'User'
 const schema = new Schema({
@@ -16,8 +17,14 @@ const schema = new Schema({
   },
   email: {
     type: String,
+    trim: true,
     required: true,
-    trim: true
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
   },
   name: {
     type: String,
