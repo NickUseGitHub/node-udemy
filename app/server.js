3a58d13aa1f30f2e3d17a711807fb7420d7b9d81
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-17 15:41:37 +0700

    serve only static

--- app/server.js
+++ app/server.js
@@ -1,21 +1,21 @@
 import express from 'express'
 import path from 'path'
-import routes from './routes'
 import bodyParser from 'body-parser'
-import middlewares from './middlewares'
+// import routes from './routes'
+// import middlewares from './middlewares'
 
-import './config'
+// import './config'
 // import './db/mongoose'
 
 const app = new express()
 
 //middlewares
-middlewares.map(md => app.use(md))
-app.use(bodyParser.json())
+// middlewares.map(md => app.use(md))
+// app.use(bodyParser.json())
 
 //routes app 
-routes.map(route => app.use(route))
-app.use(express.static(path.resolve(__dirname, 'public')))
+// routes.map(route => app.use(route))
+app.use(express.static(path.resolve(__dirname, './../public')))
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
