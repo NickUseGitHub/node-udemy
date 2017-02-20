4dbfcb2497b34735654964f9f99d60f65bb51082
Chaiwat Tungtongsoontorn <chaiwat@sanook.com>
2017-02-20 15:05:06 +0700

    assert user login has header token

--- app/test-script/server/user-route.test.js
+++ app/test-script/server/user-route.test.js
@@ -136,6 +136,7 @@ describe('----- Route:USER -----', () => {
         .post('/user/login')
         .send({email: tempUsers[0].email, password: tempUsers[0].password})
         .expect(200)
+        .expect(res => expect(res.headers[HEADER_AUTH]).toExist())
         .end((err, res) => {
           if (err) {
             return done(err)
